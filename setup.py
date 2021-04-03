35880a967efc8a86063996bdad1a323dfd5ba8cc
Radu Marginean <radu.marg@gmail.com>
2021-04-03 12:15:42 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
 setup(
   name = 'uranium-quantum',
   packages = ['uranium_quantum/circuit_composer', 'uranium_quantum/circuit_exporter'],  
-  version = '0.1.4', 
+  version = '0.1.5', 
   license='MIT',
   description = 'Support libraries for the Uranium quantum computing platform.',
   long_description=long_description,
@@ -19,7 +19,7 @@ setup(
   author = 'Radu Marginean',
   author_email = 'radu.marg@gmail.com',
   url = 'https://github.com/radumarg/uranium-quantum',
-  download_url = 'https://github.com/radumarg/uranium-quantum/archive/refs/tags/v0.1.4.tar.gz',
+  download_url = 'https://github.com/radumarg/uranium-quantum/archive/refs/tags/v0.1.5.tar.gz',
   keywords = ['quantum', 'computing', 'uranium platform'],  
   install_requires=[            
           'click',
