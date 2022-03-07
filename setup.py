0800af6a1f627c29f6856c18e6ad035817492d6e
Radu Marginean <radu.marg@gmail.com>
2022-03-07 11:02:01 +0200

    Bump version in setup file.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
 setup(
   name = 'uranium-quantum',
   packages = ['uranium_quantum/circuit_composer', 'uranium_quantum/circuit_exporter'],  
-  version = '0.2.0', 
+  version = '0.2.1',
   license='MIT',
   description = 'Support libraries for the Uranium quantum computing platform.',
   long_description=long_description,
