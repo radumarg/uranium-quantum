6f0b8367d674c9987e0bc88e16599d4e67436b16
Radu Marginean <radu.marg@gmail.com>
2022-05-22 11:21:39 +0300

    Update version of click.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
 setup(
   name = 'uranium-quantum',
   packages = ['uranium_quantum/circuit_composer', 'uranium_quantum/circuit_exporter'],  
-  version = '0.3.0',
+  version = '0.3.1',
   license='MIT',
   description = 'Support libraries for the Uranium quantum computing platform (https://uranium.transilvania-quantum.org/).',
   long_description=long_description,
