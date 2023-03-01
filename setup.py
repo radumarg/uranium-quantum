5f4fb50c6973f5ab5b0bc1250c8ae72c60ed556d
Radu Marginean <radu.marg@gmail.com>
2023-03-01 12:30:43 +0200

    New version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
 setup(
   name = 'uranium-quantum',
   packages = ['uranium_quantum/circuit_composer', 'uranium_quantum/circuit_exporter'],  
-  version = '0.3.13',
+  version = '0.3.14',
   license='MIT',
   description = 'Support libraries for the Uranium quantum computing platform (https://uranium.transilvania-quantum.org/).',
   long_description=long_description,
@@ -19,7 +19,7 @@ setup(
   author = 'Radu Marginean',
   author_email = 'radu.marg@gmail.com',
   url = 'https://github.com/radumarg/uranium-quantum',
-  download_url = 'https://github.com/radumarg/uranium-quantum/releases/tag/v0.3.13',
+  download_url = 'https://github.com/radumarg/uranium-quantum/releases/tag/v0.3.14',
   keywords = ['quantum', 'computing', 'uranium platform'],  
   install_requires=[            
           'click',
